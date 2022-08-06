3d3356dfcaf56bcb620be044c9733ffb45758268
KYBee <kyb3634@naver.com>
2022-08-07 00:05:54 +0900

    김영빈 : 1021

--- KYBee/baekjoon/src/p1021/MainDeque.java
+++ KYBee/baekjoon/src/p1021/MainDeque.java
@@ -4,18 +4,16 @@ import java.util.*;
 import java.io.*;
 import java.util.stream.IntStream;
 
-public class Main {
-
+public class MainDeque {
     static int N, M;
     static int trial;
     static int totalCnt;
     static int[] outList;
 
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("src/p1021/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Queue<Integer> q = new LinkedList<>();
+        Deque<Integer> dq = new LinkedList<>();
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
@@ -26,23 +24,20 @@ public class Main {
             outList[i] = Integer.parseInt(st.nextToken());
         }
 
-        q.addAll(IntStream.rangeClosed(1, N).collect(LinkedList::new, List::add, List::addAll));
+        dq.addAll(IntStream.rangeClosed(1, N).collect(LinkedList::new, List::add, List::addAll));
 
         while (trial < M) {
             int current = outList[trial++];
             int cnt = 0;
 
-            while (current != q.peek()) {
-                q.offer(q.poll());
+            while (current != dq.peek()) {
+                dq.add(dq.pollFirst());
                 //왼쪽으로 감
                 cnt++;
             }
-
-            totalCnt += Math.min(cnt, q.size() - cnt);
-
-            q.poll();
+            totalCnt += Math.min(cnt, dq.size() - cnt);
+            dq.pop();
         }
-
         System.out.println(totalCnt);
     }
 }
