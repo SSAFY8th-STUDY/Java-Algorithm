c62adf1105089fa9923eb88db5d8085bcc9973f5
ghs93 <ghs93@kakao.com>
2022-10-04 23:26:59 +0900

    221004

--- ghs93/baekjoon/Algorithm/src/P20160/Main.java
+++ ghs93/baekjoon/Algorithm/src/P20160/Main.java
@@ -24,8 +24,8 @@ public class Main {
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		
-		long[] YD = new long[10];
-		int[] MD = new int[10];
+		long[] YD = new long[10]; //야쿠르트 아줌마 이동거리
+		int[] MD = new int[10]; //나의 이동거리
 		Arrays.fill(YD, Integer.MAX_VALUE);
 		Arrays.fill(MD, Integer.MAX_VALUE);
 		
@@ -34,6 +34,7 @@ public class Main {
 			adjList[i] = new ArrayList<>();
 		}
 		
+		//무향그래프 입력
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			
@@ -45,14 +46,20 @@ public class Main {
 			adjList[to].add(new int[] {from, weight});
 		}
 		
+		//방문 순서
 		st = new StringTokenizer(br.readLine());
 		int[] visitList = new int[10];
 		for (int i = 0; i < 10; i++) {
 			visitList[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		//내 출발 지점
 		int mStart = Integer.parseInt(br.readLine());
 		
+		//야쿠르트 아줌마의 시작점을 기준으로 갈 수 있는 모든 곳의 최단거리 중
+		//다음 방문할 곳의 거리를 YD에 저장
+		//이후 다음 방문할 곳을 기준으로 갈 수 있는 모든 곳의 최단거리를 구하며
+		//YD완성 시키기
 		YD[0] = 0;
 		int yStart = visitList[0], yIdx = 0;
 		for (int i = 1; i < 10; i++) {
@@ -66,8 +73,10 @@ public class Main {
 			yIdx = i;
 		}
 		
+		//나의 출발점에서 갈 수 있는 최단거리
 		MD = dijkstra(mStart);
 		
+		//내가 야쿠르트 아줌마보다 빨리 도착하는 지점의 정점번호
 		int min = Integer.MAX_VALUE;
 		for (int i = 0; i < 10; i++) {
 			int cur = visitList[i];
@@ -79,22 +88,28 @@ public class Main {
 		System.out.println(min == Integer.MAX_VALUE ? -1 : min);
 	}
 
+	//다익스트라를 통한 최단경로
 	private static int[] dijkstra(int start) {
 		PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-		int[] D = new int[V+1];
+		int[] D = new int[V+1]; //임의의 정점을 기준으로한 모든 정점들과의 최단거리
 		Arrays.fill(D, Integer.MAX_VALUE);
 		
+		//시작 정점의 최단거리는 0으로 초기화
 		q.add(new int[] {start, 0});
 		D[start] = 0;
 		
 		while(!q.isEmpty()) {
 			int[] vt = q.poll();
 			
+			//지금까지의 최단거리 보다 vt[0]로 가는 거리가 클 경우 반복문을 넘어간다.
 			if(vt[1] > D[vt[0]]) continue;
 			
+			//해당 정점과 연결된 모든 정점
 			for(int[] v : adjList[vt[0]]) {
+				//해당 정점까지 누적된 거리와 앞으로 갈 거리의 합
 				int cost = D[vt[0]] + v[1];
 				
+				//위에서 구한 cost가 지금까지 누적된 거리보다 작을 경우 최단거리 재설정
 				if(D[v[0]] > cost) {
 					D[v[0]] = cost;
 					q.add(new int[] {v[0], cost});
